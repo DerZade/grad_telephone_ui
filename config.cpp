f55e645f6770a6d5a09b111bb4c3b050b25da78a
DerZade <DerZade@gmail.com>
2017-01-19 15:06:34 +0100

    inital

---
+++ config.cpp
@@ -0,0 +1,15 @@
+class CfgPatches {
+	class grad_telephone_ui {
+		units[] = {};
+		weapons[] = {};
+		requiredVersion = 0.1;
+		requiredAddons[] = {"a3_ui_f"};
+		version = "0.1";
+		author = "DerZade";
+	};
+};
+
+#include "CfgFunctions.hpp"
+
+#include "defines.hpp"
+#include "ui.hpp"
